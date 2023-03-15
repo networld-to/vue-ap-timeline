e9864942759523f02f860cf4784f993a1a5afe68
Alex Oberhauser <alex@oberhauser.global>
2023-03-14 20:27:31 -0400

    Adding loading spinner to timeline

--- src/components/VueApTimeline.vue
+++ src/components/VueApTimeline.vue
@@ -51,6 +51,14 @@ export default defineComponent({
 </script>
 
 <template>
+  <div class="spin-container" v-if="!posts.length">
+    <div class="spin" id="loader"></div>
+    <div class="spin" id="loader2"></div>
+    <div class="spin" id="loader3"></div>
+    <div class="spin" id="loader4"></div>
+    <span id="text">LOADING...</span>
+  </div>
+
   <PostEntry
     :data-bs-theme="theme"
     v-for="(post, index) in posts"
@@ -61,7 +69,7 @@ export default defineComponent({
 </template>
 
 <style>
-[data-bs-theme=dark] {
+[data-bs-theme='dark'] {
   --post-bg: #313543;
   --content-color: #babdff;
   --post-bottom-border: #fff;
@@ -74,7 +82,7 @@ export default defineComponent({
   --post-box-shadow-2: rgba(0, 0, 0, 0.07);
 }
 
-[data-bs-theme=light] {
+[data-bs-theme='light'] {
   --post-bg: #fff;
   --content-color: #212529;
   --post-bottom-border: #212529;
@@ -86,4 +94,80 @@ export default defineComponent({
   --post-box-shadow-1: rgba(50, 50, 93, 0.9);
   --post-box-shadow-2: rgba(0, 0, 0, 0.07);
 }
+
+.spin-container {
+  text-align: center;
+  padding-top: 100px;
+  height: 375px;
+}
+.spin {
+  border: 3px solid #0d4b9f;
+  width: 200px;
+  height: 200px;
+  margin: 0 auto;
+  border-radius: 50%;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  animation: rotate 2s cubic-bezier(0.26, 1.36, 0.74, -0.29) infinite;
+}
+#loader2 {
+  border: 3px solid #e0edff;
+  width: 220px;
+  height: 220px;
+  position: relative;
+  top: -216px;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  animation: rotate2 2s cubic-bezier(0.26, 1.36, 0.74, -0.29) infinite;
+}
+#loader3 {
+  border: 3px solid #005cdc;
+  width: 240px;
+  height: 240px;
+  position: relative;
+  top: -452px;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  animation: rotate 2s cubic-bezier(0.26, 1.36, 0.74, -0.29) infinite;
+}
+#loader4 {
+  border: 3px solid #94b6e5;
+  width: 260px;
+  height: 260px;
+  position: relative;
+  top: -708px;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  animation: rotate2 2s cubic-bezier(0.26, 1.36, 0.74, -0.29) infinite;
+}
+@keyframes rotate {
+  0% {
+    transform: rotateZ(-360deg);
+  }
+  100% {
+    transform: rotateZ(0deg);
+  }
+}
+@keyframes rotate2 {
+  0% {
+    transform: rotateZ(360deg);
+  }
+  100% {
+    transform: rotateZ(0deg);
+  }
+}
+#text {
+  color: #0d4b9f;
+  font-family: Arial;
+  font-size: 18px;
+  font-weight: 600;
+  position: relative;
+  top: -857px;
+}
+
+.loading a {
+    font-size: 1.5rem;
+    padding: 15px;
+}
+
 </style>
